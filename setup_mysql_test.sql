88ae361afcc443ab35d3cbf0762e176223a0a5a5
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-20 02:35:32 +0300

    CORRECT TASK #4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,13 @@
--- 3. MySQL setup development
+-- 4. MySQL setup test
 /*
-A database hbnb_dev_db
-A new user hbnb_dev (in localhost)
-The password of hbnb_dev should be set to hbnb_dev_pwd
-hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
-hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
-If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
+Write a script that prepares a MySQL server for the project:
+
+A database hbnb_test_db
+A new user hbnb_test (in localhost)
+The password of hbnb_test should be set to hbnb_test_pwd
+hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
+hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
+If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
 */
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
@@ -13,13 +15,13 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 USE hbnb_test_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_test '@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT USAGE ON *.* TO 'hbnb_test '@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_test '@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test '@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
 
 
 FLUSH PRIVILEGES;
