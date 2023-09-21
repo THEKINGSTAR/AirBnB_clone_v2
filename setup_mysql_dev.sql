2d455320d975234fabbccffab75fa17a1a4a4936
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-20 19:05:58 -0700

    still fixing task 6

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,19 +7,12 @@ hbnb_dev should have all privileges on the database hbnb_dev_db (and only this d
 hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
 */
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
-
-USE hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
-
 FLUSH PRIVILEGES;
