02deb80f20b01d327531da13c6a3b862a67a9611
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-06 03:07:04 +0300

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,1 +1,32 @@
-testing
+#!/usr/bin/env bash
+#0. Prepare your web servers
+sudo apt-get update
+sudo apt-get install nginx -y
+
+sudo ufw allow 'Nginx HTTP'
+
+
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+
+target='/data/web_static/releases/test/'
+link='/data/web_static/current'
+if [ -L "link" ];
+	then
+	sudo rm "$link"
+fi
+sudo ln -s "$target" "$link"
+
+sudo chown -R ubuntu:ubuntu /data/
+
+
+sudo sed -i 's|location /hbnb_static/ {|location /hbnb_static/ {\n    alias /data/web_static/current/;|' /etc/nginx/sites-available/default
+
+
+sudo fuser -k 80/tcp
+sudo nginx -t
+sudo systemctl restart nginx
