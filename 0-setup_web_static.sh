b371ce3c17d20c81edbafa1113c3f67eedfe0d8f
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-06 04:23:06 +0300

    last try for TASK 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,17 +3,13 @@
 
 sudo apt-get update
 sudo apt-get install nginx -y
-
 sudo ufw allow 'Nginx HTTP'
-
-
 sudo mkdir /data/
 sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 echo "PREPARING THE WEB SERVER" | sudo tee -a /data/web_static/releases/test/index.htm
-
 target='/data/web_static/releases/test/'
 link='/data/web_static/current'
 if [ -L "link" ];
@@ -21,13 +17,9 @@ if [ -L "link" ];
 	sudo rm "$link"
 fi
 sudo ln -s "$target" "$link"
-
-sudo chown -R ubuntu:ubuntu /data/
-
-
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i 's|location /hbnb_static/ {|location /hbnb_static/ {\n    alias /data/web_static/current/;|' /etc/nginx/sites-available/default
-
-
 sudo fuser -k 80/tcp
 sudo nginx -t
 sudo systemctl restart nginx
+sudo service nginx start
---
+++ 1-pack_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+1. Compress before sending
+Fabric script that generates a .tgz
+archive from the contents of the web_static folder
+"""
+def do_pack():
+    """
+    generates a .tgz archive from the contents of the web_static
+    """
+
