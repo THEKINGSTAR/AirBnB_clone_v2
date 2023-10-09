f6de8ae9220f0403ed1048d40bbc2e2ded5e341f
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-09 03:44:38 +0300

    FIX TASK # 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,16 +9,24 @@ sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
+
 echo "PREPARING THE WEB SERVER" | sudo tee -a /data/web_static/releases/test/index.htm
+
 target='/data/web_static/releases/test/'
 link='/data/web_static/current'
+
 if [ -L "link" ];
 	then
 	sudo rm "$link"
 fi
+
 sudo ln -s "$target" "$link"
+
 sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/web_static/releases/test/index.html
+
 sudo sed -i 's|location /hbnb_static/ {|location /hbnb_static/ {\n    alias /data/web_static/current/;|' /etc/nginx/sites-available/default
+
 sudo fuser -k 80/tcp
 sudo nginx -t
 sudo systemctl restart nginx
