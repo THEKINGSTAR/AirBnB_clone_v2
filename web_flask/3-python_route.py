f06ceba7fa0169f206da5ca5c90598d3ca441934
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-23 02:29:36 +0300

    3. Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", methods=['GET'], strict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+
+@app.route("/c/<text>", methods=['GET'], strict_slashes=False)
+def txt(text):
+    text = text.replace("_", " ")
+    return (f"C {escape(text)}")
+
+
+@app.route("/python/", methods=['GET'], strict_slashes=False)
+@app.route("/python/<text>", methods=['GET'], strict_slashes=False)
+def Python(text="is cool"):
+    text = text.replace("_", " ")
+    return (f"Python {escape(text)}")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
